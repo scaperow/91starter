4991dbfde09b9c4d9585cf2f5364a985630a0f44
scaperow <scaperow@hotmail.com>
2017-09-18 18:17:25 +0800

    mailgun config

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,10 +19,12 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
+		{ label: '首页', key: 'home', href: '/' },
+		/*
 		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
+		*/
 	];
 	res.locals.user = req.user;
 	next();
